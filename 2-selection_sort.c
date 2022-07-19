d98c420c8194bf3611f219c82283a8ae7339b06a
katman-Generation <tkatema5@gmail.com>
2022-07-18 17:05:44 -0700

    submitting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,31 +2,30 @@
 
 /**
  * selection_sort - sorts an array using selective sort
- * @array: an array to be sorted 
+ * @array: an array to be sorted
  * @size: size of the array
- * 
+ *
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i,j,t, swap;
-
-    for (i = 0; i < size - 1; i++)
-    {
-        t = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[t])
-            {
-                t = j;
-            }
-        if (t != i)
-        {
-            swap = array[i];
-            array[i] = array[t];
-            array[t] = swap;
-            print_array(array, size);
-        }
-        }
-
-    }
+	size_t i, j, t, swap;
+	
+	for (i = 0; i < size - 1; i++)
+	{
+		t = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[t])
+			{
+				t = j;
+			}
+		}
+		if (t != i)
+		{
+			swap = array[i];
+			array[i] = array[t];
+			array[t] = swap;
+			print_array(array, size);
+		}
+	}
 }
